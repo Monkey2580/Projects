fb8bec1c867bcf9e5f81a665911f5ffb7e45dac7
george marcell <george.marcell@ist.id>
2024-10-20 12:31:17 +0700

    Improve Calculation, variable usage and Time to process

--- N-Queen Genetic Algorithm/N-queenGeneticAlg.py
+++ N-Queen Genetic Algorithm/N-queenGeneticAlg.py
@@ -1,21 +1,23 @@
 import random
+import copy
+from itertools import chain
 from time import *
 from time import process_time
 
+
 list_of_boards = []
-attack = 0
 
 
 class EmptyBoard(object): # Makes an empty board the size of queen x queen
     def __init__(self):
-        self.Board = []
-        for i in range(Nqueen):
-            self.Board.append([0] * Nqueen)
+        self.QLoc = []
+        self.Attack = 0
 
 
 class Board(object):
     def __init__(self):
-        self.Board = []
+        self.QLoc = []
+        self.Attack = 0
 
 
 def make_board(size):
@@ -26,61 +28,63 @@ def make_board(size):
 def random_board(board, row, nqueen): # randomize the location of the queens in the board
     for i in range(row):
         rd = random.randrange(0, nqueen)
-        board.Board[i][rd] = 1
+        board.QLoc.append(rd)
 
 
 def check_fittest(board): # check how many queens are attacking each other
-    global attack
-
     for i in board:
-        for j in range(len(i.Board)):
-            for k in range(len(i.Board[j])):
-                queen_attacked(i.Board, j, k)
-        i.Board.append(attack)
-        attack = 0
+        queen_attacked(i)
 
 
 def sort_list(board):
-    board.sort(key=lambda board: board.Board[Nqueen])
+    board.sort(key=lambda board: board.Attack)
 
 
 def crossover(board): # Taking 2 parents and crossing their queens placement
     child_list = []
     for i in range(population):
-        p1 = random.randrange(1, int(population / 2))
-        p2 = random.randrange(1, int(population / 2))
-        rd = random.randrange(1, len(board[0].Board) - 1)
-        # print(rd)
+        p1 = random.randrange(0, int(population / 2))
+        p2 = random.randrange(0, int(population / 2))
+        rd = random.randrange(0, len(board[0].QLoc))
+
         child_list.append(Board())
-        for j in range(rd):
-            child_list[i].Board.append(board[p1].Board[j])
-        for j in range(rd, len(board[p2].Board) - 1):
-            child_list[i].Board.append(board[p2].Board[j])
+        child_list[i].QLoc.append(copy.deepcopy(board[p1].QLoc[slice(rd)]))
+        child_list[i].QLoc.append(copy.deepcopy(board[p2].QLoc[slice(rd,len(board[p2].QLoc))]))
+        child_list[i].QLoc = list(chain.from_iterable(child_list[i].QLoc))
+        
     return child_list
 
 
-def mutation(board): # introducing some mutation to the board so there's a new variable
+
+def mutation(board,nQueen): # introducing some mutation to the board so there's a new variable
     random_picker = random.randrange(0, len(board))
-    random_col = random.randrange(0, len(board[random_picker].Board) - 1)
-    random_row = random.randrange(0, len(board[random_picker].Board) - 1)
-    for i in range(len(board[random_picker].Board[random_row])):
-        board[random_picker].Board[random_row][i] = 0
-    for i in range(len(board[random_picker].Board[random_row])):
-        board[random_picker].Board[random_row][random_col] = 1
+    random_col = random.randrange(0, len(board[random_picker].QLoc))
+    random_row = random.randrange(0, nQueen)
+    
+    board[random_picker].QLoc[random_row]=random_col
 
-
-def queen_attacked(board, row, col): # the actual calculation of checking which queen is attacking which queen
-    global attack
-    if board[row][col] == 1:
-        for i in range(row, -1, -1):
-            if board[i][col] == 1 and (i != row):
-                attack += 1
-        for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-            if board[i][j] == 1 and (i != row and j != col):
-                attack += 1
-        for i, j in zip(range(row, -1, -1), range(col, len(board[col]), 1)):
-            if board[i][j] == 1 and (i != row and j != col):
-                attack += 1
+                
+def queen_attacked(board): # the actual calculation of checking which queen is attacking which queen
+    passed = []
+    attack = 0
+    
+    for qLoc in board.QLoc:
+        if(qLoc in passed):
+            attack +=1
+        else :
+            passed.append(qLoc)
+             
+    for row in range(len(board.QLoc)):  
+        for secRow in range(row+1,len(board.QLoc)):
+            deltaRow = abs(secRow - row )
+            deltaCol = abs(board.QLoc[row] - board.QLoc[secRow])
+            
+            if(deltaRow == deltaCol):
+                attack+=1
+                
+    board.Attack=attack
+            
+        
 
 print("This program is not yet optimized so the bigger the input the longer it takes to calculate")
 BxB=input("Enter the amount of queens in the board (min:4) :")
@@ -96,35 +100,36 @@ for i in range(population):
 check_fittest(list_of_boards)
 j = 10
 sort_list(list_of_boards)
-crossover(list_of_boards)
+list_of_boards = crossover(list_of_boards)
 gen = 0
 t1_start = process_time()
-while j != 0:
+solution =0
+while j!=0:
 
     list_of_boards = crossover(list_of_boards)
     check_fittest(list_of_boards)
     for i in list_of_boards:
-        if i.Board[Nqueen] == 0:
+        if i.Attack == 0:
             sort_list(list_of_boards)
             j = 0
+            solution = i
+            break
         else:
-            mutation(list_of_boards)
-            mutation(list_of_boards)
-            sort_list(list_of_boards)
-
+            mutation(list_of_boards,Nqueen)
     gen += 1
+    
 t1_stop = process_time()
 print("the visualisation of the chessboard:")
 print("-------------------------------------")
-for i in range(len(list_of_boards[0].Board)-1):
-    print(list_of_boards[0].Board[i])
+for row in range(Nqueen):
+    for col in range(Nqueen):
+        if(col == solution.QLoc[row]):
+            print(' 1 ',end='')
+        else :
+            print(' 0 ',end='')
+    print()
 print("-------------------------------------")
-output=[]
-for i in range(len(list_of_boards[0].Board)-1):
-    for j in range(len(list_of_boards[0].Board[0])):
-        if list_of_boards[0].Board[j][i]==1:
-            output.append(j+1)
-print("The desired output:",output)
+print("The desired output:",solution.QLoc)
 print("answer found on generation:", gen)
 print("Elapsed time:", t1_stop, t1_start)
 if (t1_stop - t1_start) <59.99:
